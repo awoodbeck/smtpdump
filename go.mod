4a315885fb5225fbcceee32b97405ab1678845ea
Adam Woodbeck <adam@woodbeck.net>
2020-03-11 21:08:00 -0400

    Add TLS support

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/awoodbeck/smtpdump
+
+go 1.14
+
+require (
+	github.com/fatih/color v1.9.0
+	github.com/mhale/smtpd v0.0.0-20181125220505-3c4c908952b8
+)
--- smtpdump.go
+++ smtpdump.go
@@ -14,19 +14,18 @@ import (
 	"time"
 
 	c "github.com/fatih/color"
-
-	"github.com/awoodbeck/smtpd"
+	"github.com/mhale/smtpd"
 )
 
 var (
-	addr      string
-	color     bool
-	debug     bool
-	extension string
-	hostname  string
-	output    string
-	verbose   bool
-
+	addr        = flag.String("addr", "127.0.0.1:2525", "Listen address:port")
+	cert        = flag.String("cert", "", "PEM-encoded certificate")
+	color       = flag.Bool("color", true, "color debug output")
+	extension   = flag.String("extension", "eml", "Saved file extension")
+	hostname    string
+	pkey        = flag.String("key", "", "PEM-encoded private key")
+	output      = flag.String("output", "", "Output directory (default to current directory)")
+	verbose     = flag.Bool("verbose", false, "verbose output")
 	readPrintf  = c.New(c.FgGreen).Printf
 	writePrintf = c.New(c.FgCyan).Printf
 )
@@ -37,12 +36,7 @@ func init() {
 		log.Fatalln(err)
 	}
 	flag.StringVar(&hostname, "hostname", hn, "Server host name")
-	flag.StringVar(&addr, "addr", "127.0.0.1:2525", "Listen address:port")
-	flag.StringVar(&output, "output", "", "Output directory (default to current directory)")
-	flag.StringVar(&extension, "extension", "eml", "Saved file extension")
-	flag.BoolVar(&color, "color", true, "color debug output")
-	flag.BoolVar(&debug, "debug", false, "debug output")
-	flag.BoolVar(&verbose, "verbose", false, "verbose output")
+	flag.BoolVar(&smtpd.Debug, "debug", false, "debug output")
 }
 
 func main() {
@@ -52,46 +46,62 @@ func main() {
 		log.Fatalln("Hostname cannot be empty")
 	}
 
-	if debug {
-		smtpd.Debug = true
-		verbose = true
+	if smtpd.Debug {
+		*verbose = true
 
-		if !color {
+		if !*color {
 			readPrintf = fmt.Printf
 			writePrintf = fmt.Printf
 		}
 	}
 
 	var err error
-	if output == "" {
-		output, err = os.Getwd()
+	if *output == "" {
+		*output, err = os.Getwd()
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
-	_, err = os.Stat(output)
+	_, err = os.Stat(*output)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	srv := &smtpd.Server{
-		Addr:    addr,
-		Handler: outputHandler(output, extension, verbose),
-		Appname: "SMTPDump",
+		Addr:        *addr,
+		Appname:     "SMTPDump",
+		AuthHandler: authHandler,
+		Handler:     outputHandler(*output, *extension, *verbose),
 		LogRead: func(_, _, line string) {
 			line = strings.Replace(line, "\n", "\n  ", -1)
-			readPrintf("  %s\n", line)
+			_, _ = readPrintf("  %s\n", line)
 		},
 		LogWrite: func(_, _, line string) {
 			line = strings.Replace(line, "\n", "\n  ", -1)
-			writePrintf("  %s\n", line)
+			_, _ = writePrintf("  %s\n", line)
 		},
+		HandlerRcpt: rcptHandler,
 	}
 
-	if verbose {
-		log.Printf("Listening on %q ...\n", addr)
+	if *cert != "" && *pkey != "" {
+		err = srv.ConfigureTLS(*cert, *pkey)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Enabled TLS support")
 	}
+
+	if *verbose {
+		log.Printf("Listening on %q ...\n", *addr)
+	}
+
 	log.Fatalln(srv.ListenAndServe())
+}
+
+// authHandler logs credentials and always returns true.
+func authHandler(origin net.Addr, mechanism string, username []byte, password []byte, shared []byte) (bool, error) {
+	log.Printf("[AUTH] User: %q; Password: %q\n", username, password)
+	return true, nil
 }
 
 // outputHandler is called when a new message is received by the server.
@@ -128,6 +138,11 @@ func outputHandler(output, ext string, verbose bool) smtpd.Handler {
 	}
 }
 
+func rcptHandler(origin net.Addr, from string, to string) bool {
+	log.Printf("[RCPT] %q => %q\n", from, to)
+	return true
+}
+
 // randFile returns a pointer to a new file or an error.  If
 // dir is empty, the temporary directory is used.
 func randFile(dir, prefix, suffix string) (*os.File, error) {
@@ -142,7 +157,7 @@ func randFile(dir, prefix, suffix string) (*os.File, error) {
 
 	// Make a reasonable number of attempts to find a unique file name.
 	for i := 0; i < 10000; i++ {
-		// Quick and Dirty congruential generator from Numerical Recipies.
+		// Quick and Dirty congruential generator from Numerical Recipes.
 		r := int(time.Now().UnixNano()+int64(os.Getpid()))*1664525 + 1013904223
 		fn := fmt.Sprintf("%s_%d.%s", prefix, r, suffix)
 		name := filepath.Join(dir, fn)
